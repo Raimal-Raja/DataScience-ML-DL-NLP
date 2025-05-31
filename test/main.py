201f938e4b21a517263793f856115546b19bcebc
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-05-31 09:46:48 +0500

    updated

--- test/main.py
+++ test/main.py
@@ -1,3 +1,14 @@
-import pyjokes 
-joke = pyjokes.get_joke()
-print(joke)
+# import pyjokes 
+# joke = pyjokes.get_joke()
+# print(joke)
+
+
+#line continuation
+# sum =  1+3+\
+#     4+4
+# print(sum)
+
+
+# multiple line statement
+x = 3; y = 10; z = x+y
+print(z)
