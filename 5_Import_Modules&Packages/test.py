d0dd8274f45e5a8003d6af2464be30e3263d09a0
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-06-07 08:36:13 +0500

    updated

--- 5_Import_Modules&Packages/test.py
+++ 5_Import_Modules&Packages/test.py
@@ -1,2 +1,13 @@
-from package.maths import addition
-print(addition(1,2))
+# from package.maths import addition
+# print(addition(1,2))
+
+import csv
+with open('example.csv', mode='w', newline='')  as file:
+    writer = csv.writer(file)
+    writer.writerow(['name', 'age'])
+    writer.writerow(['Raimal', 20])
+    
+with open('example.csv', mode='r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
