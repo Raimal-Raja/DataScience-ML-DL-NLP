1ae541834ce15b01618adff97be9cff253e4adf9
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-06-09 20:25:52 +0500

    updated

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 ## file handling and exception handling
-try:
-    file = open('example.txt', 'r')
-    content = file.read()
-    a=b
-    print(content)
-except FileNotFoundError:
-    print('The file does not exists')
+# try:
+#     file = open('example.txt', 'r')
+#     content = file.read()
+#     a=b
+#     print(content)
+# except FileNotFoundError:
+#     print('The file does not exists')
     
-except Exception as e:
-    print(e)
+# except Exception as e:
+#     print(e)
     
-finally:
-    if 'file' in locals() and not file.closed():
-        file.close()
-        print('file closed')
+# finally:
+#     if 'file' in locals() and not file.closed():
+#         file.close()
+#         print('file closed')
+
+
+## Generators
+def sqaure(n):
+    for i in range(3):
+        yield i**2
+        
+print(sqaure(3))
+for i in sqaure(3):
+    print(i)
