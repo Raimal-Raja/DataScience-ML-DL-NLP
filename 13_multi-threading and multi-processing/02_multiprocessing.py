302c06f6e65b71fc65f4d0fe49e7e31b5a07059d
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-06-19 01:43:22 +0500

    updated

--- 13_multi-threading and multi-processing/02_multiprocessing.py
+++ 13_multi-threading and multi-processing/02_multiprocessing.py
@@ -17,21 +17,25 @@ def cude_number():
         time.sleep(1.5)
         print(f"Cude: {i*i*i}")
         
-##  create 2 processes
-p1 = multiprocessing.Process(target=square_number)
-p2 = multiprocessing.Process(target=cude_number)
+        
+# entry point
+if __name__ =='__main__':
+        
+            ##  create 2 processes
+            p1 = multiprocessing.Process(target=square_number)
+            p2 = multiprocessing.Process(target=cude_number)
 
 
-t = time.time()
-## start the process
-p1.start()
-p2.start()
+            t = time.time()
+            ## start the process
+            p1.start()
+            p2.start()
 
 
-# wait for the process to complete
-p1.join()
-p2.join()
+            # wait for the process to complete
+            p1.join()
+            p2.join()
 
-stop_time = time.time()-t
+            stop_time = time.time()-t
 
-print(stop_time)
+            print(stop_time)
