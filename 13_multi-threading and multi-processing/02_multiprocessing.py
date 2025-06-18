d44ae946005195781e3fa8ae79c9998b97488392
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-06-19 01:38:54 +0500

    updated

---
+++ 13_multi-threading and multi-processing/02_multiprocessing.py
@@ -0,0 +1,37 @@
+# Process that run in parallel
+# CPU-bound task that are heavy on CPU usage(e.g, mathematical computations, data processing)
+# Parallel execution - Multiple cores of the CPU
+
+
+import multiprocessing
+import time
+
+def square_number():
+    for i in range(5):
+        time.sleep(1)
+        print(f"Square: {i*i}")
+        
+        
+def cude_number():
+    for i in range(5):
+        time.sleep(1.5)
+        print(f"Cude: {i*i*i}")
+        
+##  create 2 processes
+p1 = multiprocessing.Process(target=square_number)
+p2 = multiprocessing.Process(target=cude_number)
+
+
+t = time.time()
+## start the process
+p1.start()
+p2.start()
+
+
+# wait for the process to complete
+p1.join()
+p2.join()
+
+stop_time = time.time()-t
+
+print(stop_time)
