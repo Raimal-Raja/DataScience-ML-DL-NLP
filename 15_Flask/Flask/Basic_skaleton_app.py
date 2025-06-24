9240bfc8ab3d73bda165e8e29c58d5de6b5b6735
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-06-24 09:06:13 +0500

    updated

--- 15_Flask/Flask/Basic_skaleton_app.py
+++ 15_Flask/Flask/Basic_skaleton_app.py
@@ -1,1 +1,13 @@
 from flask import Flask
+
+'''It creates an instance of the Flask class,
+which will be your WSGI (Web Server Gateway  Interface) application'''
+
+app = Flask(__name__)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
