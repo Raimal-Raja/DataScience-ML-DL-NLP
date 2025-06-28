8cb28f4dd4bfc578c3c40dfd06827e7cfd38e8d0
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-06-28 18:31:37 +0500

    updated

--- 16_streamlit_app/app.py
+++ 16_streamlit_app/app.py
@@ -5,3 +5,19 @@ import pandas as pd
 
 ## Title of the application
 st.title("Hello Streamlit")
+
+# Display a simple text
+st.write('This is a simple txt')
+
+## create a simple dataframe
+df = pd.DataFrame({
+    'first column': [1,2,3,4,5],
+    'Second column': [10,20,30,40,50]
+})
+
+# display the dataframe
+st.write('here is the dataframe')
+st.write(df)
+
+# create a line chart
+st.line_chart()
